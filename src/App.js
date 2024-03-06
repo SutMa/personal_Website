77645cd8aa4ca50c33fb54b811f4fd74a44c7c06
Sut <odineyez@gmail.com>
2024-03-06 17:14:47 -0600

    Added current works

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <Work/>
+      <Current/>
       <Experience/>
       <Education/>
       <Contact/>
--- src/components/current.jsx
+++ src/components/current.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import handydandy from '../pictures/handydandyWork.png'
-import style from '../styles//current.module.css'
+import React from 'react';
+import handydandy from '../pictures/handydandyWork.png';
+import style from '../styles/current.module.css';
 
 function Current(){
     return (
-        <div className='root'>
-            <div className="container">
+        <div className={style.root}>
+            <div className={style.content}>
                 <h2 className={style.currentHeader}>Currently Working on my Pet Project</h2>
-                <img className={style.currentWorks} src={handydandy} alt=""/>
+                <h3 className={style.subheader}>Stay tuned for more...</h3>
+                <div className={style.imageWrapper}>
+                    <img className={style.currentWorks} src={handydandy} alt=""/>
+                </div>
             </div>
         </div>
-    )
+    );
 }
-
 
 export default Current;
